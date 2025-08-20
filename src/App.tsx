7e6c918fd0eab8548693483d7699a9105fc1c857
rndall <96964503+rndall@users.noreply.github.com>
2025-08-20 17:02:46 +0800

    feat: create navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { Link, NavLink, Outlet, useLocation } from "react-router";
+
+import { ShoppingCart } from "lucide-react";
+
 function App() {
-  return <div>App</div>;
+  const location = useLocation();
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="shop">Shop</NavLink>
+            </li>
+            {location.pathname.includes('shop') &&
+              <li>
+                <Link to="shop/cart"><ShoppingCart /></Link>
+              </li>
+            }
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import App from "./App";
+import Home from "./routes/Home";
+import Shop from "./routes/Shop";
+import ShoppingCart from "./routes/ShoppingCart";
 
 const routes = [
   {
-    index: true,
     Component: App,
+    children: [
+      { index: true, Component: Home },
+      {
+        path: "shop", children: [
+          { index: true, Component: Shop },
+          { path: 'cart', Component: ShoppingCart }]
+      },
+    ],
   },
 ];
 
