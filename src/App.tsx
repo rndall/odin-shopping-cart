0f8c52196f820d357c6cfc0b5d88db3ca038a961
rndall <96964503+rndall@users.noreply.github.com>
2025-08-23 18:40:09 +0800

    feat: set fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import "./App.css"
+
 import { Outlet } from "react-router";
 
 import Navbar from "./components/Navbar/Navbar";
-
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Navbar />
 
-      <main>
+      <main className="main">
         <Outlet />
       </main>
     </>
