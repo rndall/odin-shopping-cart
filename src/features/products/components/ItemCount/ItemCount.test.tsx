34815ed841fabf32ed3f9c0591073a499cc2b7d9
rndall <96964503+rndall@users.noreply.github.com>
2025-09-03 09:29:46 +0800

    test: create ItemCount tests

---
+++ src/features/products/components/ItemCount/ItemCount.test.tsx
@@ -0,0 +1,56 @@
+import { describe, expect, test, vi } from "vitest";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ItemCount from "./ItemCount";
+
+describe("ItemCount component", () => {
+  test("render quantity", () => {
+    const { container } = render(
+      <ItemCount quantity={1} setQuantity={() => {}} />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  describe("buttons", () => {
+    test("call setQuantity after increment button click", async () => {
+      const setQuantity = vi.fn();
+      const user = userEvent.setup();
+
+      render(<ItemCount quantity={1} setQuantity={setQuantity} />);
+
+      const incrementBtn = screen.getByRole("button", { name: "+" });
+
+      await user.click(incrementBtn);
+
+      expect(setQuantity).toHaveBeenCalled();
+      expect(setQuantity).toHaveBeenCalledWith(2);
+    });
+
+    test("call setQuantity after decrement button click", async () => {
+      const setQuantity = vi.fn();
+      const user = userEvent.setup();
+
+      render(<ItemCount quantity={5} setQuantity={setQuantity} />);
+
+      const decrementBtn = screen.getByRole("button", { name: "-" });
+
+      await user.click(decrementBtn);
+
+      expect(setQuantity).toHaveBeenCalledWith(4);
+    });
+
+    test("do not allow quantity to drop below 1", async () => {
+      const user = userEvent.setup();
+      const setQuantity = vi.fn();
+
+      render(<ItemCount quantity={1} setQuantity={setQuantity} />);
+
+      const decrementButton = screen.getByRole("button", { name: "-" });
+
+      await user.click(decrementButton);
+
+      expect(setQuantity).toHaveBeenCalledWith(1);
+    });
+  });
+});
