f2ef2cd255b7657abd12e8bbac48a1d1ae5b180b
rndall <96964503+rndall@users.noreply.github.com>
2025-09-18 12:08:10 +0800

    refactor: Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,46 +1,41 @@
-import styles from "./Navbar.module.css";
+import { NavLink, useLocation } from "react-router"
 
-import { NavLink, useLocation } from "react-router";
+import { ShoppingCart } from "lucide-react"
 
-import { ShoppingCart } from "lucide-react";
+const navLinks = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "Shop",
+    route: "shop",
+  },
+]
 
 interface NavbarProps {
-  cartItemCount: number;
+  cartItemCount: number
 }
 
 export default function Navbar({ cartItemCount }: NavbarProps) {
-  const location = useLocation();
-
-  const navLinks = [
-    {
-      name: "Home",
-      route: "/",
-    },
-    {
-      name: "Shop",
-      route: "shop",
-    },
-  ];
+  const location = useLocation()
 
   const showShoppingCartIcon = () => {
-    const { pathname } = location;
-    return pathname.includes("shop") || pathname.includes("products");
-  };
+    const { pathname } = location
+    return pathname.includes("shop") || pathname.includes("products")
+  }
 
   return (
-    <header className={styles.header}>
-      <div
-        className={`${styles.navContainer}`}
-        style={{ position: "relative" }}
-      >
+    <header className="bg-background fixed top-0 z-10 w-full py-5 sm:py-8">
+      <div className="relative mx-auto flex items-center justify-center 2xl:max-w-[96rem]">
         <nav>
-          <ul className={styles.navList}>
+          <ul className="flex justify-center space-x-4">
             {navLinks.map(({ name, route }, index) => (
               <li key={index}>
                 <NavLink
                   preventScrollReset
                   to={route}
-                  className={styles.navLink}
+                  className="hover:text-foreground-hover"
                 >
                   {name}
                 </NavLink>
@@ -52,13 +47,13 @@ export default function Navbar({ cartItemCount }: NavbarProps) {
           <NavLink
             preventScrollReset
             to="shop/cart"
-            className={styles.cartLink}
+            className="hover:text-foreground-hover absolute right-1/14"
           >
-            <div className={styles.cartContainer}>
+            <div className="relative">
               <ShoppingCart size={20} />
               {cartItemCount > 0 && (
-                <div className={styles.cartItems}>
-                  <p>{cartItemCount}</p>
+                <div className="bg-primary absolute -top-[38%] left-4/5 flex h-4 min-w-4 items-center justify-center rounded-full px-0.75 py-0.5 font-bold text-white">
+                  <p className="text-xs">{cartItemCount}</p>
                 </div>
               )}
             </div>
@@ -66,5 +61,5 @@ export default function Navbar({ cartItemCount }: NavbarProps) {
         )}
       </div>
     </header>
-  );
+  )
 }
