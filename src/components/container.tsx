ed2125ef45734cc564fdf9026f4f0b85c4f1f322
rndall <96964503+rndall@users.noreply.github.com>
2025-09-18 10:03:03 +0800

    feat: create container component

---
+++ src/components/container.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils"
+
+interface ContainerProps extends React.ComponentProps<"div"> {
+  children: React.ReactNode
+}
+
+export default function Container({ children, className }: ContainerProps) {
+  return (
+    <div className={cn("container", "px-4", "sm:px-8", "mx-auto", className)}>
+      {children}
+    </div>
+  )
+}
