7283254071cfea15b1358b5eee74327e9eec3f60
rndall <96964503+rndall@users.noreply.github.com>
2025-09-19 13:23:27 +0800

    feat: add max width breakpoints to conatiner component

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -6,7 +6,12 @@ interface ContainerProps extends React.ComponentProps<"div"> {
 
 export default function Container({ children, className }: ContainerProps) {
   return (
-    <div className={cn("container, px-4, sm:px-8, mx-auto", className)}>
+    <div
+      className={cn(
+        "container mx-auto px-4 sm:max-w-[40rem] sm:px-8 md:max-w-[48rem] lg:max-w-[64rem] xl:max-w-[80rem] 2xl:max-w-[96rem]",
+        className,
+      )}
+    >
       {children}
     </div>
   )
